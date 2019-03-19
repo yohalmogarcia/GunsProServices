d1b0578c150f8bece995e2beea35b5a47732cb4c
Unknown <yohalmo.garcia@gmail.com>
2019-03-18 20:56:27 -0600

    GunsProWS
    Init

---
+++ GunsProWS/src/main/java/com/sys/gunspro/GunsProWsApplication.java
@@ -0,0 +1,13 @@
+package com.sys.gunspro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunsProWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GunsProWsApplication.class, args);
+	}
+
+}
